e1ac275ff4699aff7a34e59ef1e0a24a4d86a020
Sergey <140523602+S-n0brain@users.noreply.github.com>
2025-06-25 05:59:13 +0900

    Update settings.py

--- lessons/settings.py
+++ lessons/settings.py
@@ -161,5 +161,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_USE_TLS = True
 EMAIL_USE_SSL = True
 EMAIL_PORT = 465
-EMAIL_HOST_USER = "koltafvhdgv@gmail.com"
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
